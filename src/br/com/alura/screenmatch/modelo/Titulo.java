6f7a1ed3c80ecf9250d499ae551b5d22fe0ff6ba
gonzalezarielle <gonzalezarielle@gmail.com>
2023-05-08 18:55:40 -0300

    Gravando arquivos.

--- src/br/com/alura/screenmatch/modelo/Titulo.java
+++ src/br/com/alura/screenmatch/modelo/Titulo.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.modelo;
 
+import br.com.alura.screenmatch.excessao.ErroDeConversaoDeAnoException;
+
 public class Titulo implements Comparable<Titulo> {
     private String nome;
     private int anoDeLancamento;
@@ -11,6 +13,16 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length() > 4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 3));
     }
 
     public int getTotalDeAvaliacoes() {
@@ -67,5 +79,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "(Nome: " + nome +
+                ", Ano de Lançamento= " + anoDeLancamento
+                + ", Duração: " + duracaoEmMinutos + " minutos.)";
     }
 }
